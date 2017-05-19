29c86a1ba3dce243e477e4f69c9e461eaab10161
jpdias <jpdias@live.com.pt>
2017-05-19 16:55:43 +0100

    file structure

--- client/app.js
+++ client/app.js
@@ -1,12 +1,11 @@
 const request = require('request');
-const rp = require('request-promise');
 const express = require('express');
 const bodyParser = require('body-parser');
+const snapshots = require('./snapshots.js');
 
 const httpPort = process.env.HTTP_PORT || 1337;
 
 const serverMineUrl = 'http://localhost:3001/mineBlock';
-const serverBlocks = 'http://localhost:3001/blocks';
 
 class Permission {
   constructor(entity, level, resource) {
@@ -16,19 +15,7 @@ class Permission {
   }
 }
 
-class BlockchainSnapshot {
-  constructor(index, lastSnapshotHash, lastIndex, lastHash, rules) {
-    this.index = index;
-    this.lastSnapshotHash = lastSnapshotHash;
-    this.lastIndex = lastIndex;
-    this.lastHash = lastHash;
-    this.rules = rules;
-  }
-}
-
 let batch = [];
-
-const snapshots = [new BlockchainSnapshot(0, 0, 0, 0, [])];
 
 const PermissionLevel = {
   NONE: 0,
@@ -79,47 +66,6 @@ const commitBatch = () => {
   }
 };
 
-const requestBlocks = {
-  uri: serverBlocks,
-  headers: { 'User-Agent': 'Request-Promise' },
-  json: true, // Automatically parses the JSON string in the response
-};
-
-const getSnapshot = async () => {
-  const blocks = await rp(requestBlocks);
-
-  const lastblock = blocks[blocks.length - 1];
-
-  const lastSnapshot = snapshots[snapshots.length - 1];
-
-  let rules = [];
-  for (let j = 0; j < blocks.length; j += 1) {
-    const currentBlock = blocks[j].data;
-    for (let k = 0; k < currentBlock.length; k += 1) {
-      const newSnapshot = rules.filter((obj) => {
-        if (
-            obj.entity === currentBlock[k].entity &&
-            obj.resource === currentBlock[k].resource
-        ) {
-          return false;
-        }
-        return true;
-      });
-      newSnapshot.push(currentBlock[k]);
-      rules = newSnapshot;
-    }
-  }
-
-  const newSnapshotBlock = new BlockchainSnapshot(lastSnapshot.index + 1,
-                                lastSnapshot.hash,
-                                lastblock.index,
-                                lastblock.hash,
-                                rules);
-
-  snapshots.push(newSnapshotBlock);
-  return rules;
-};
-
 const initHttpServer = () => {
   const app = express();
   app.use(bodyParser.json());
@@ -160,10 +106,14 @@ const initHttpServer = () => {
   });
 
   app.get('/snapshot', (req, res) => {
-    Promise.resolve(getSnapshot()).then((snapshot) => {
+    Promise.resolve(snapshots.getSnapshot()).then((snapshot) => {
       console.log(snapshot);
       res.json(snapshot);
     });
+  });
+
+  app.get('/getSnapshotChain', (req, res) => {
+    res.json(snapshots.getSnapshotChain);
   });
 
   /* Open server */
---
+++ client/snapshots.js
@@ -0,0 +1,67 @@
+const crypto = require('crypto');
+const rp = require('request-promise');
+
+const serverBlocks = 'http://localhost:3001/blocks';
+
+const calculateHash = (index, lastSnapshotHash, lastIndex, lastHash, rules) => crypto.createHash('sha256')
+    .update(index + lastSnapshotHash + lastIndex + lastHash + rules).digest('hex');
+
+class BlockchainSnapshot {
+  constructor(index, lastSnapshotHash, lastIndex, lastHash, rules) {
+    this.index = index;
+    this.lastSnapshotHash = lastSnapshotHash;
+    this.lastIndex = lastIndex;
+    this.lastHash = lastHash;
+    this.rules = rules;
+    this.hash = calculateHash(index, lastSnapshotHash, lastIndex, lastHash, rules);
+  }
+}
+
+const requestBlocks = {
+  uri: serverBlocks,
+  headers: { 'User-Agent': 'Request-Promise' },
+  json: true, // Automatically parses the JSON string in the response
+};
+
+const snapshots = [new BlockchainSnapshot(0, 0, 0, 0, [])];
+
+const getChainSnapshot = async () => {
+  const blocks = await rp(requestBlocks);
+
+  const lastblock = blocks[blocks.length - 1];
+
+  const lastSnapshot = snapshots[snapshots.length - 1];
+
+  let rules = [];
+  for (let j = 0; j < blocks.length; j += 1) {
+    const currentBlock = blocks[j].data;
+    for (let k = 0; k < currentBlock.length; k += 1) {
+      const newSnapshot = rules.filter((obj) => {
+        if (
+            obj.entity === currentBlock[k].entity &&
+            obj.resource === currentBlock[k].resource
+        ) {
+          return false;
+        }
+        return true;
+      });
+      newSnapshot.push(currentBlock[k]);
+      rules = newSnapshot;
+    }
+  }
+
+  const newSnapshotBlock = new BlockchainSnapshot(lastSnapshot.index + 1,
+                                lastSnapshot.hash,
+                                lastblock.index,
+                                lastblock.hash,
+                                rules);
+
+  snapshots.push(newSnapshotBlock);
+  return rules;
+};
+
+module.exports = {
+  getSnapshot: getChainSnapshot,
+  getSnapshotChain: snapshots,
+};
+
