d15c2378bc894849e884d38aeb164c036ec20942
jpdias <jpdias@live.com.pt>
2017-05-22 12:10:55 +0100

    update tests

--- test/client_spec.js
+++ test/client_spec.js
@@ -15,6 +15,12 @@ const rules = [
     resource: 'server3' },
 ];
 
+const rules1 = {
+  entity: 'FEUP',
+  level: 0,
+  resource: 'server1',
+};
+
 describe('Get snapshot', () => {
   it('should return snapshot', (done) => {
     server
@@ -64,6 +70,46 @@ describe('Get snapshot', () => {
     .end((err, res) => {
       if (err) done(err);
       res.body.should.be.containDeep(rules);
+      done();
+    });
+  });
+});
+
+describe('Add access rule', () => {
+  it('should respond with 200', (done) => {
+    server
+      .post('/addRule')
+      .send(rules1)
+      .expect(200)
+      .end((err) => {
+        if (err) done(err);
+        done();
+      });
+  });
+});
+
+describe('Get commitRules', () => {
+  it('should commit new rule', (done) => {
+    server
+    .get('/commitRules')
+    .expect(200) // THis is HTTP response
+    .end((err, res) => {
+      if (err) done(err);
+      done();
+    });
+  });
+});
+
+describe('Get snapshot', () => {
+  it('should return snapshot with changed rule', (done) => {
+    server
+    .get('/snapshot')
+    .expect('Content-type', /json/)
+    .expect(200) // THis is HTTP response
+    .end((err, res) => {
+      if (err) done(err);
+      res.body.should.not.be.containDeep(rules);
+      res.body.should.be.containDeep([rules1]);
       done();
     });
   });
